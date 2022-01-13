c8363ccaebd53fd914cbe3b84e55a802c14947f1
Simon Szustkowski <mail@simonszu.de>
2022-01-13 13:05:04 +0100

    Can handle nbsp in the title now

--- webview.js
+++ webview.js
@@ -6,9 +6,9 @@ module.exports = Ferdi => {
     var unread = 0;
     var match = [];
     // Get value of <title> tag where in case of new feed elements the number of elements appear
-    const titleValue = document.querySelector('title').text;
+    const titleValue = document.querySelector('title').text.replace(" ", "");
     // Extract the number from the tag
-    match = titleValue.match(/\d+/);
+    match = titleValue.match(/[\d\s]+/);
     // Some logic to handle the match groups
     unread = match != null && match.length > 0 ? match[0] : 0;
     // Set unread msgs badge
